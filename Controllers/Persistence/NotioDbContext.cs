79faea4591dd39f29bb3b93001c8f3988cd30c73
Xokhezi <nemec.nuovo@gmail.com>
2023-01-11 06:14:38 +0100

    first commit

---
+++ Controllers/Persistence/NotioDbContext.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Notio2._0.Models;
+
+namespace Notio2._0.Controllers.Persistence
+{
+    public class NotioDbContext:DbContext
+    {
+        public NotioDbContext(DbContextOptions<NotioDbContext>options):base(options)
+        {
+        }
+            public DbSet<Article> Articles { get; set; }
+        
+    }
+}
