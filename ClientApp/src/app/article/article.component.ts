f354a5e7d2fd949f92a06d09489056f90112dab0
Xokhezi <110103053+Xokhezi@users.noreply.github.com>
2023-02-01 10:39:53 +0100

    photos to article page

--- ClientApp/src/app/article/article.component.ts
+++ ClientApp/src/app/article/article.component.ts
@@ -1,3 +1,4 @@
+import { PhotosService } from './../services/photos.service';
 import { ArticlesService } from './../services/articles.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
@@ -8,19 +9,19 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./article.component.css']
 })
 export class ArticleComponent implements OnInit {
+  photos:any;
   article:any;
   id:any;
   cursorOn=false;
-  constructor( private active:ActivatedRoute,private service:ArticlesService) { }
+  constructor( private active:ActivatedRoute,private service:ArticlesService,private photoService:PhotosService) { }
 
   ngOnInit(): void {
-    this.active.paramMap.subscribe(params=>{
+        this.active.paramMap.subscribe(params=>{
       this.id = params.get('id');
       this.article=this.service.GetArticle(this.id)
-      .subscribe(a=>{
-        this.article=a;
-        console.log(this.article);
-      });        
+      .subscribe(a=>this.article=a);   
+      this.photoService.getPhotos(this.id)
+      .subscribe(p=>this.photos=p);     
   });   
   }
   switchCursorOn()
